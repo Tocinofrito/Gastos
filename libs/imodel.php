5cfdd3e4f4d8ca426a764b8aa241dcede0b8883f
Ismael-pc <chichamalo@gmail.com>
2022-04-17 13:58:49 -0500

    usermodel implementado

--- libs/imodel.php
+++ libs/imodel.php
@@ -1,6 +1,8 @@
 <?php
 //Permite definir métodos a ser implementados
 //Aqui no va lógica, solo los métodos con sus parámetros
+//Solo see implementará cuando ocupemos  hacer un crud con algo
+
 interface IModel{
     public function save();
     public function getAll();
