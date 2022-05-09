f940bebb8bda14d3434f18ecb583e33a47225d80
Ismael-pc <chichamalo@gmail.com>
2022-05-09 08:24:48 -0500

    .

--- models/loginmodel.php
+++ models/loginmodel.php
@@ -10,9 +10,22 @@ class LoginModel extends Model{
     function login($username, $password){
         try {
             //Accedemos  a dbname
+            
             $query = $this->prepare('SELECT FROM users WHERE username = : username');
             $query->execute(['username' => $username]);
+            //Si existe usuario con el username accedemos a los datos de la
+            //Como no puede haber más de 1 usuario con el mismo nombre pos hacemos la pregunta si existe 1
             if($query-rowCount() == 1){
+
+                $item = $query->fetch(PDO::FETCH_ASSOC);
+
+                $user = new UserModel();
+                //from inicializa el userModel con los datos enviados, en este caso los que mandamos de la anteior consulta del login
+                $user->from($item);
+                if(password_verify($password, $user-getPassword())){
+                    error_log('LoginModel::login->success');
+                    return $user;
+                }
                 
             }
             
