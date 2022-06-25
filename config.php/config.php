95cd6119f9b75fa568f63643acb6ac83d5276714
Ismael-pc <chichamalo@gmail.com>
2022-06-25 15:54:53 -0500

    archivos iniciales

--- config.php/config.php
+++ config.php/config.php
@@ -1,5 +1,5 @@
 <?php
-define('URL', 'http://localhost:80/expense');
+define('URL', 'http://localhost:80/expense/');
 define('HOST','localhost');
 define('PASSWORD','Paradis32$');
 define('DB','expenseapp' );
